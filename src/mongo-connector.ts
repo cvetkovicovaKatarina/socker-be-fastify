37e1163098a8b05ec332f3ac37c9ee165a83e439
Katarina Cvetkovicova <katarina.cvetkovicova@gmail.com>
2022-01-16 18:16:43 +0100

    Fastify setup, first route.

--- src/mongo-connector.ts
+++ src/mongo-connector.ts
@@ -1,10 +1,10 @@
-import fastifyPlugin from 'fastify-plugin'
-import fastifyMongo from 'fastify-mongodb'
+import fastifyPlugin from 'fastify-plugin';
+import fastifyMongo from 'fastify-mongodb';
 
-async function mongoConnector (fastify: any, options: any) {
-  fastify.register(fastifyMongo, {
-    url: 'mongodb://localhost:27017/test_database'
-  })
+async function mongoConnector(fastify: any, options: any) {
+	fastify.register(fastifyMongo, {
+		url: 'mongodb://localhost:27017/test_database',
+	});
 }
 
-export default fastifyPlugin(mongoConnector)
+export default fastifyPlugin(mongoConnector);
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,11 @@
-import { FastifyReply, FastifyRequest } from "fastify"
+import { FastifyReply, FastifyRequest } from 'fastify';
 
-async function routes (fastify: any, options: any) {
-	const collection = fastify.mongo.db.collection('test_collection')
-  
+async function routes(fastify: any, options: any) {
+	const collection = fastify.mongo.db.collection('test_collection');
+
 	fastify.post('/game', async (request: FastifyRequest, reply: FastifyReply) => {
-	  return { message: 'Game was created.' }
-	})
-  }
-  
-  export default routes
+		return { message: 'Game was created.' };
+	});
+}
+
+export default routes;
